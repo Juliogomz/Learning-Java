3987507ba0f134de50f40a28ede58decc49e55ae
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-09-02 11:08:37 -0300

    Java-Update

--- Fundamentos/Variaveis/Declaracao Caracteres/src/Caractere.java
+++ Fundamentos/Variaveis/Declaracao Caracteres/src/Caractere.java
@@ -1,5 +1,11 @@
 public class Caractere {
     public static void main(String[] args) throws Exception {
-       
+        char letra;
+        letra = 'j';
+        System.out.println(letra);
+        //O tipo de variável char armazena apenas um caractere. 
+        //OBS: Quando for utilizar char é preciso colocar o caractere entre aspas simples.
+
+
     }
 }
