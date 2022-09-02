85b67ba88e169fa3781fa107a80c12ceccfb0708
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-09-02 11:28:05 -0300

    Java-Update

---
+++ Fundamentos/Variaveis/Declaracao String/src/Strin.java
@@ -0,0 +1,11 @@
+public class Strin {
+    public static void main(String[] args) throws Exception {
+        String diasemana;
+        diasemana = "segunda";
+        System.out.println(diasemana);
+        //A variável do tipo string armazena uma sequência de caracteres.
+	    //OBS: Quando for utilizar String é preciso colocar a sequência de caracteres 
+        //entre aspas duplas. 
+
+    }
+}
