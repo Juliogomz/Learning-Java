bd1671cc7acd53af7b00477d33f8b6d43aa4107e
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-09-02 11:08:19 -0300

    Java-Update

---
+++ Fundamentos/Variaveis/Declaracao Booleana/src/Booleana.java
@@ -0,0 +1,8 @@
+public class Booleana {
+    public static void main(String[] args) throws Exception {
+        boolean estadenoite;
+        estadenoite = false;
+        System.out.println(estadenoite);
+        //A variável do tipo booleana armazena valores do tipo verdadeiro(True) ou falso(False).
+    }
+}
