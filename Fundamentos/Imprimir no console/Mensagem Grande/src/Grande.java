59c2d75a51debc02a07aa4e16941457fa00fcc05
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-09-02 22:35:42 -0300

    Java-Update

---
+++ Fundamentos/Imprimir no console/Mensagem Grande/src/Grande.java
@@ -0,0 +1,16 @@
+public class Grande {
+    public static void main(String[] args) throws Exception {
+
+        String nome = "Julio";
+        int idade = 18;
+        char sexo = 'M';
+        float altura = 1.83f;
+        double peso = 80;
+
+        //É possível se colocar várias variáveis no mesmo console por meio 
+        //da concatenação.
+
+        System.out.println(nome + " eh um programador de " + idade + " anos de idade do sexo " + sexo +". Que pesa " + peso +"kg e tem " + altura + " de altura.");
+    }
+}
+        //Dessa forma é possível agrupar as variáveis da forma que quiser no console.
---
+++ Fundamentos/Imprimir no console/Mensagem/src/Mensagem.java
@@ -0,0 +1,21 @@
+public class Mensagem {
+    public static void main(String[] args) throws Exception {
+
+        String nome = "Julio";
+        int idade = 18;
+        char sexo = 'M';
+        float altura = 1.83f;
+        double peso = 80;
+        boolean programador = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Altura: " + altura);
+        System.out.println("Peso: " + peso);
+        System.out.println("Programador: " + programador);
+
+        //Usa-se aspas duplas para se inserir textos no console.
+        //Para concatenar esses textos com as variáveis se usa o símbolo "+".
+    }
+}
