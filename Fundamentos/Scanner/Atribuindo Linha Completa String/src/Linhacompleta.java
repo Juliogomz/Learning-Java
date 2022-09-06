7c84c9ccde60f1506440e9c58ea5db042bc38d44
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-09-06 18:39:08 -0300

    Java-Update

---
+++ Fundamentos/Scanner/Atribuindo Linha Completa String/src/Linhacompleta.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//Para usar o scanner é preciso se inserir as importações de classe “import java.util.Scanner;”
+
+public class Linhacompleta {
+    public static void main(String[] args) throws Exception {
+
+        String nome;
+        Scanner leitor = new Scanner(System.in);
+
+        leitor.useDelimiter("[\r\n]+");
+
+        //Usa-se "leitor.useDelimiter("[\r\n]+");" para que o espaço não seja mais um separador de String.
+
+        
+        System.out.println("Digite o seu nome completo: ");
+
+        nome = leitor.next();
+
+        //nome é a variável que vai ser preenchida
+        //Leitor é a variável criada com a classe Scanner para realizar o processo
+        //“new Scanner()” serve para instanciar a variável
+        //“(System.in)” serve para determinar que o usuário deverá digitar os valores 
+
+        System.out.println("O seu nome completo é:\n" + nome);
+    }
+}
