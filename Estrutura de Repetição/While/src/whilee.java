87ab174b81256ec5ce91c1e0cbfe953a79bb090a
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-09-29 12:55:07 -0300

    Java-Update

---
+++ Estrutura de Repetição/While/src/whilee.java
@@ -0,0 +1,11 @@
+public class whilee {
+    public static void main(String[] args) throws Exception {
+        int contador = 0 ;
+
+        while(contador < 5){
+        System.out.println("Essa mensagem se repetirá 5 vezes"); 
+        contador++;
+
+        }
+    }
+}
