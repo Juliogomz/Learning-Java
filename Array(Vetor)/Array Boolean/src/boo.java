de55c5dd9f37a213478f741bce89180e6633fb75
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-10-03 13:46:27 -0300

    Java-Update

---
+++ Array(Vetor)/Array Boolean/src/boo.java
@@ -0,0 +1,10 @@
+public class boo {
+    public static void main(String[] args) throws Exception {
+        boolean[] num = new boolean[2];
+        num[0] = true;
+        num[1] = false;
+
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+    }
+}
