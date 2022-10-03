b2b241bf710c10d2ae1b0cbd005907a8cc5a08db
Julio Gomes <106027379+Juliogomz@users.noreply.github.com>
2022-10-03 14:30:25 -0300

    Java-Update

---
+++ Array(Vetor)/Todos os Valores/src/todos.java
@@ -0,0 +1,20 @@
+public class todos {
+    public static void main(String[] args) throws Exception {
+        int[] numeros = new int[10];
+        numeros[0] = 10;
+        numeros[1] = 9;
+        numeros[2] = 8;
+        numeros[3] = 7;
+        numeros[4] = 6;
+        numeros[5] = 5;
+        numeros[6] = 4;
+        numeros[7] = 3;
+        numeros[8] = 2;
+        numeros[9] = 1;
+
+        for(int contador = 0; contador < 10; contador++){
+            System.out.println(numeros[contador]);
+        }
+
+    }
+}
